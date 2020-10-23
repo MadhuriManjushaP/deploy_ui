9c06580ae224e537d47735944198ab20e87cfe50
unknown <madhuri.manjusha9@gmail.com>
2020-10-23 18:25:18 +0530

    Updates

---
+++ precache-manifest.6c8c5995f45a9dc6721738c97114bb67.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cacd9dd89c73fd1233aea55a9c2224a0",
+    "url": "/deploy_ui/index.html"
+  },
+  {
+    "revision": "fa80a55a2a83a1ec3174",
+    "url": "/deploy_ui/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b097ee9930f5da587be9",
+    "url": "/deploy_ui/static/js/2.ae714951.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/deploy_ui/static/js/2.ae714951.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa80a55a2a83a1ec3174",
+    "url": "/deploy_ui/static/js/main.f5b082ed.chunk.js"
+  },
+  {
+    "revision": "1ae6e87694090ec1934c",
+    "url": "/deploy_ui/static/js/runtime-main.31abb8e0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/deploy_ui/static/media/logo.5d5d9eef.svg"
+  }
+]);
